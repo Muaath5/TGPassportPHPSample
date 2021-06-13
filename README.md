348e29100faee2ff4af9642df31d15b46394b776
Muaath Alqarni <muaath1428@hotmail.com>
2021-06-13 12:23:58 +0300

    Support Heroku upload

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # PHP Sample for Telegram Passport
 
 1. Create bot
-2. Fill config.php
-3. Open {BASE_URL}/webhook.php?\_set\_webhook=1 once to set webhook
-4. Try {BASE_URL}/example.php
+2. Fill `config.php`, Or [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?templete=https://github.com/TelegramMessenger/TGPassportPHPSample)
+3. Open `{BASE_URL}/webhook.php?\_set\_webhook=1` once to set webhook
+4. Try `{BASE_URL}/example.php`
 
 More info about [Telegram Passport](https://core.telegram.org/passport)
 
--- config.php
+++ config.php
@@ -1,8 +1,42 @@
 <?php
 
-define('BOT_ID',          'XXXXXX');                              // place id of your bot here
-define('BOT_USERNAME',    'XXXXXXXXXX');                          // place username of your bot here
+$bot_token       = getenv('BOT_TOKEN');
+$bot_username    = getenv('BOT_USERNAME');
+$bot_private_key = getenv('BOT_PRIVATE_KEY');
+$bot_public_key  = getenv('BOT_PUBLIC_KEY');  
+$hmac_secret     = getenv('HMAC_SECRET');
+$files_dir_name  = getenv('FILES_DIR_NAME');
+$base_dir_name   = getenv('BASE_DIR');
+$base_url        = 'https://' . $_SERVER['SERVER_NAME'] . $base_dir_name;
+$files_base_url  = $base_url . '/' . $files_dir_name;
+$files_dir       = $_SERVER['DOCUMENT_ROOT'] . '/' . $files_dir_name;
+
+// Heroku app case
+if ($bot_token !== false && $bot_private_key !== false && $hmac_secret !== false && $bot_username !== false)
+{
+    list($bot_id, $token) = explode(':', BOT_TOKEN, 2);
+    
+    // Define variables
+    define('BOT_ID', $bot_id);
+    define('BOT_TOKEN', $bot_token);
+    define('BOT_USERNAME', $bot_username);
+    define('BOT_PUBLIC_KEY', $bot_public_key);
+    define('BOT_PRIVATE_KEY', $bot_private_key);
+    define('HMAC_SECRET', $bot_public_key);
+    define('BASE_URL', $base_url);
+    define('FILES_BASE_URL', $files_base_url);
+    define('FILES_DIR', $files_dir);
+    unset($bot_id);
+    unset($bot_username);
+    unset($bot_token);
+}
+
 define('BOT_TOKEN',       'XXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXX');   // place bot token of your bot here, KEEP IT SECRET!
+define('BOT_USERNAME',    'XXXXXXXXXX');                          // place username of your bot here
+list($bot_id, $token) = explode(':', BOT_TOKEN, 2);
+define('BOT_ID', $bot_id);
+unset($bot_id);
+unset($token);
 
 define('BOT_PUBLIC_KEY',  '-----BEGIN PUBLIC KEY----- ...');      // place public key of your bot here
 define('BOT_PRIVATE_KEY', '-----BEGIN RSA PRIVATE KEY----- ...'); // place private key of your bot here, KEEP IT SECRET!
@@ -15,5 +49,3 @@ define('FILES_DIR',       '/var/www/files/');                     // place dir w
 
 $MC = new Memcache;
 $MC->connect('localhost', 11211);
-
-?>
